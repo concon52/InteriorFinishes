622358615f7b99221d8118c7db7702d8c3756cad
concon52 <conorsullivan@nevada.unr.edu>
2016-11-30 17:33:37 -0800

    made separate head, header, and footer files

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+
+function outputFooter()
+{
+
+echo '
+    <footer class="footer">
+        <div class="container">
+            <p class="text-muted">Place sticky footer content here.</p>
+        </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>
+    window.jQuery || document.write(\'<script src="../../assets/js/vendor/jquery.min.js"><\/script>\')
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>';
+
+}
+
+?>
